48aec1eb1dd946a10479a6c6262cda0f2aecbacc
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2021-02-23 21:59:30 +0100

    add new properties to result class

--- src/main/java/se/payerl/haws/types/Result.java
+++ src/main/java/se/payerl/haws/types/Result.java
@@ -1,13 +1,199 @@
 package se.payerl.haws.types;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Result {
     private Context context;
-
-    public Result(Context context) {
-        this.context = context;
-    }
+    private double latitude;
+    private double longitude;
+    private int elevation;
+    @SerializedName("unit_system")
+    private UnitSystem unitSystem;
+    @SerializedName("location_name")
+    private String locationName;
+    @SerializedName("time_zone")
+    private String timeZone;
+    private List<String> components;
+    @SerializedName("config_dir")
+    private String configDir;
+    @SerializedName("whitelist_external_dirs")
+    private List<String> whitelistExternalDirs;
+    @SerializedName("allowlist_external_dirs")
+    private List<String> allowlistExternalDirs;
+    @SerializedName("allowlist_external_urls")
+    private List<String> allowlistExternalUrls;
+    private String version;
+    @SerializedName("config_source")
+    private String configSource;
+    @SerializedName("safe_mode")
+    private boolean safeMode;
+    private String state;
+    @SerializedName("external_url")
+    private String externalUrl;
+    @SerializedName("internal_url")
+    private String internalUrl;
 
     public Context getContext() {
-        return this.context;
+        return context;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getElevation() {
+        return elevation;
+    }
+
+    public UnitSystem getUnitSystem() {
+        return unitSystem;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public List<String> getComponents() {
+        return components;
+    }
+
+    public String getConfigDir() {
+        return configDir;
+    }
+
+    public List<String> getWhitelistExternalDirs() {
+        return whitelistExternalDirs;
+    }
+
+    public List<String> getAllowlistExternalDirs() {
+        return allowlistExternalDirs;
+    }
+
+    public List<String> getAllowlistExternalUrls() {
+        return allowlistExternalUrls;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getConfigSource() {
+        return configSource;
+    }
+
+    public boolean isSafeMode() {
+        return safeMode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getExternalUrl() {
+        return externalUrl;
+    }
+
+    public String getInternalUrl() {
+        return internalUrl;
+    }
+
+    public Result setContext(Context context) {
+        this.context = context;
+        return this;
+    }
+
+    public Result setLatitude(double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+
+    public Result setLongitude(double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+
+    public Result setElevation(int elevation) {
+        this.elevation = elevation;
+        return this;
+    }
+
+    public Result setUnitSystem(UnitSystem unitSystem) {
+        this.unitSystem = unitSystem;
+        return this;
+    }
+
+    public Result setLocationName(String locationName) {
+        this.locationName = locationName;
+        return this;
+    }
+
+    public Result setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+        return this;
+    }
+
+    public Result setComponents(List<String> components) {
+        this.components = components;
+        return this;
+    }
+
+    public Result setConfigDir(String configDir) {
+        this.configDir = configDir;
+        return this;
+    }
+
+    public Result setWhitelistExternalDirs(List<String> whitelistExternalDirs) {
+        this.whitelistExternalDirs = whitelistExternalDirs;
+        return this;
+    }
+
+    public Result setAllowlistExternalDirs(List<String> allowlistExternalDirs) {
+        this.allowlistExternalDirs = allowlistExternalDirs;
+        return this;
+    }
+
+    public Result setAllowlistExternalUrls(List<String> allowlistExternalUrls) {
+        this.allowlistExternalUrls = allowlistExternalUrls;
+        return this;
+    }
+
+    public Result setVersion(String version) {
+        this.version = version;
+        return this;
+    }
+
+    public Result setConfigSource(String configSource) {
+        this.configSource = configSource;
+        return this;
+    }
+
+    public Result setSafeMode(boolean safeMode) {
+        this.safeMode = safeMode;
+        return this;
+    }
+
+    public Result setState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public Result setExternalUrl(String externalUrl) {
+        this.externalUrl = externalUrl;
+        return this;
+    }
+
+    public Result setInternalUrl(String internalUrl) {
+        this.internalUrl = internalUrl;
+        return this;
     }
 }
---
+++ src/main/java/se/payerl/haws/types/UnitSystem.java
@@ -0,0 +1,54 @@
+package se.payerl.haws.types;
+
+public class UnitSystem {
+    private String length;
+    private String mass;
+    private String pressure;
+    private String temperature;
+    private String volume;
+
+    public String getLength() {
+        return length;
+    }
+
+    public String getMass() {
+        return mass;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public UnitSystem setLength(String length) {
+        this.length = length;
+        return this;
+    }
+
+    public UnitSystem setMass(String mass) {
+        this.mass = mass;
+        return this;
+    }
+
+    public UnitSystem setPressure(String pressure) {
+        this.pressure = pressure;
+        return this;
+    }
+
+    public UnitSystem setTemperature(String temperature) {
+        this.temperature = temperature;
+        return this;
+    }
+
+    public UnitSystem setVolume(String volume) {
+        this.volume = volume;
+        return this;
+    }
+}
