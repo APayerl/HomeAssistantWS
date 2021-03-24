cda7811fd4cbcc0e27b2635db8d3e7a9c3a5ecab
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2021-03-24 22:39:21 +0100

    fix bug with ResultMessage class not being able to instantiate

--- src/main/java/se/payerl/haws/types/Server/ResultMessage.java
+++ src/main/java/se/payerl/haws/types/Server/ResultMessage.java
@@ -9,33 +9,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResultMessage extends ServerMessage {
-    private boolean success;
-    private List<Result> results = new ArrayList<>();
+    private Boolean success = null;
+    private List<Result> result = null;
 
-    @JsonCreator
-    public ResultMessage(@JsonProperty("id") int id,
-                         @JsonProperty("success") boolean success,
-                         @JsonProperty("results") List<Result> result) {
-        super(ServerTypes.RESULT, id);
-        this.success = success;
-        this.results = result;
-    }
-    @JsonCreator
-    public ResultMessage(@JsonProperty("id") int id,
-                         @JsonProperty("success") boolean success,
-                         @JsonProperty("results") Result result) {
-        super(ServerTypes.RESULT, id);
-        this.success = success;
-        this.results.add(result);
-    }
+//    @JsonCreator
+//    public ResultMessage(@JsonProperty("id") int id,
+//                         @JsonProperty("success") boolean success,
+//                         @JsonProperty("results") List<Result> result) {
+//        super(ServerTypes.RESULT, id);
+//        this.success = success;
+//        this.results = result;
+//    }
+//    @JsonCreator
+//    public ResultMessage(@JsonProperty("id") int id,
+//                         @JsonProperty("success") boolean success,
+//                         @JsonProperty("results") Result result) {
+//        super(ServerTypes.RESULT, id);
+//        this.success = success;
+//        this.results.add(result);
+//    }
 
     public boolean getSuccess() {
         return this.success;
     }
     public Result getResult() {
-        return this.results.get(0);
+        return this.result.get(0);
     }
     public List<Result> getResults() {
-        return this.results;
+        return this.result;
+    }
+
+    public ResultMessage setSuccess(boolean success) {
+        this.success = success;
+        return this;
+    }
+
+    public ResultMessage setResult(List<Result> results) {
+        this.result = results;
+        return this;
+    }
+
+    public ResultMessage setResult(Result result) {
+        this.result = new ArrayList<>();
+        this.result.add(result);
+        return this;
     }
 }
