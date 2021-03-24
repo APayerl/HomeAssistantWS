730309878c571f9065c3f2c781fa44fe5103dec8
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2021-03-25 00:13:45 +0100

    change DataService class

--- src/main/java/se/payerl/haws/types/ServiceData.java
+++ src/main/java/se/payerl/haws/types/ServiceData.java
@@ -1,19 +1,24 @@
 package se.payerl.haws.types;
 
 public class ServiceData {
-    private String entityId;
+    private String entityId = null;
+    private Integer code = null;
 
     public String getEntityId() {
-        return entityId;
+        return this.entityId;
+    }
+    public Integer getCode() {
+        return this.code;
     }
 
     public ServiceData setEntityId(String entityId) {
         this.entityId = entityId;
         return this;
     }
+    public ServiceData setCode(int code) {
+        this.code = code;
+        return this;
+    }
 
     public ServiceData() {}
-    public ServiceData(String entityId) {
-        this.entityId = entityId;
-    }
 }
