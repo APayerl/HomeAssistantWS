29a15693a1ab6ea01f0a26920a632f29b1a5fc3c
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2021-04-15 12:33:20 +0200

    change ServiceData class to use unspecified properties

--- src/main/java/se/payerl/haws/types/ServiceData.java
+++ src/main/java/se/payerl/haws/types/ServiceData.java
@@ -1,24 +1,48 @@
 package se.payerl.haws.types;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import se.payerl.haws.CaseHandler;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ServiceData {
-    private String entityId = null;
-    private Integer code = null;
+    @JsonIgnore
+    private Map<String, Object> attr = new TreeMap<>();
+    public ServiceData() { }
 
+    @JsonAnySetter
+    public void add(String key, Object value) {
+        this.attr.put(CaseHandler.snake_case_ToCamelCase(key), value);
+    }
+
+    @JsonAnyGetter(enabled = true)
+    public Map<String, Object> getAttributes() {
+        Map<String, Object> mMap = new TreeMap<>();
+        this.attr.entrySet().forEach(entry ->
+                mMap.put(CaseHandler.camelCaseTo_snake_case(entry.getKey()), entry.getValue()));
+        return mMap;
+    }
+
+    @JsonIgnore
+    public boolean hasAttribute(String key) {
+        return this.attr.containsKey(key);
+    }
+
+    @JsonIgnore
+    public Object getAttribute(String key) {
+        return this.attr.get(key);
+    }
+
+    @JsonIgnore
     public String getEntityId() {
-        return this.entityId;
+        return (String) this.attr.get("entityId");
     }
+
+    @JsonIgnore
     public Integer getCode() {
-        return this.code;
+        return (Integer) this.attr.get("code");
     }
-
-    public ServiceData setEntityId(String entityId) {
-        this.entityId = entityId;
-        return this;
-    }
-    public ServiceData setCode(int code) {
-        this.code = code;
-        return this;
-    }
-
-    public ServiceData() {}
 }
